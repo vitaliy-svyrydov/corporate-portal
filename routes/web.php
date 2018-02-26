d05084e90c3a03701c36903e9848e2c812b4659d
vitaliy-svyrydov <svyrydov.vitaliy@gmail.com>
2018-02-26 12:30:53 +0200

    added contacts page

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::resource('articles', 'ArticlesController', [
                                                     'parameters' => [
                                                     'articles' => 'alias'
                                                     ]]);
-Route::get('articles/cat/{cat_alias?}', ['uses' => 'ArticlesController@index','as'=>'articlesCat']);
+Route::get('articles/cat/{cat_alias?}', ['uses' => 'ArticlesController@index','as'=>'articlesCat'])->where('cat_alias','[\w-]+');
 
 Route::resource('comment','CommentController',['only'=>['store']]);
+
+Route::match(['get','post'],'/contacts',['uses'=>'ContactsController@index','as'=>'contacts']);
